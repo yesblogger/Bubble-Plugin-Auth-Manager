9aa347fca92d6d96ef46104f1edad5f8b2b4716d
cake-capital <amartya@cakecapital.com>
2025-06-16 12:56:21 +0530

    big fix

--- actions/ABQ-85152/client.js
+++ actions/ABQ-85152/client.js
@@ -1,12 +1,20 @@
 function(properties, context) {
-
-  	//Load any data
     const credentials = {
         email: properties.email,
         otp: properties.otp
     };
 
-    // Do the operation
-    return window.AuthManager.otplogin(credentials);
-
+    return window.AuthManager.otplogin(credentials)
+        .then(data => {
+            return {
+                auth_token: data.access,
+                first_name: data.user.first_name,
+                last_name: data.user.last_name,
+                email: data.user.email,
+                id: data.user.id
+            };
+        })
+        .catch(error => {
+            throw new Error(error.message || 'OTP Login failed');
+        });
 }
--- actions/ABQ-85152/server.js
+++ actions/ABQ-85152/server.js
@@ -1,24 +1,3 @@
 function(properties, context) {
-
-    //Load any data
-    const credentials = {
-        email: properties.email,
-        otp: properties.otp
-    };
-
-    // Do the operation
-    return window.AuthManager.otplogin(credentials)
-        .then(data => {
-            return {
-                auth_token: data.access,
-                first_name: data.user.first_name,
-                last_name: data.user.last_name,
-                email: data.user.email,
-                id: data.user.id
-            };
-        })
-        .catch(error => {
-            throw new Error(error.message || 'OTP Login failed');
-        });
-
+    // Leave this empty or remove the file entirely
 }
